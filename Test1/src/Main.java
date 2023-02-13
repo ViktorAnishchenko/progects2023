70267e1991261920f8587623bcd765a45a637e79
Anishchenko Viktor <m.arefeva@skillbox.by>
2023-02-13 11:07:29 +0300

    test with branch1

--- Test1/src/Main.java
+++ Test1/src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Test");
-<<<<<<< HEAD
+
         System.out.println("Branch 1");
-=======
->>>>>>> 8a97e5e (test)
-    }
+
 }
