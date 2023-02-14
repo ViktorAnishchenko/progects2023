2a1684abe26d8dd9a22ce7c70371a89cd89ac97b
Anishchenko Viktor <m.arefeva@skillbox.by>
2023-02-14 12:09:03 +0300

    branch 5

--- Test1/src/Main.java
+++ Test1/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Test");
         System.out.println("branch 4");
+        System.out.println("branch 5");
         System.out.println("Test 2");
         System.out.println("branch 3");
     }
